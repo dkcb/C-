3991f4ed79054ca0383fe15361453f1e3b9cd20d
dkcb <yes@exa.com>
2023-01-13 15:56:53 +0100

    phone1

--- CPP00/ex01/src/Phonebook.cpp
+++ CPP00/ex01/src/Phonebook.cpp
@@ -21,26 +21,6 @@ std::string	PhoneBook::Input( ) {
 			buffer.clear();
 		else
 			break ;
-		// if (buffer.empty())
-		// 	continue ;
-
 	}
 	return buffer;
 }
-
-// int main ( ){
-// 	PhoneBook	pb;
-// 	string		cmd;
-	
-// 	int	i = 1;
-
-// 	cmd = pb.Input();
-// 	while (i < 10)
-// 	{
-// 		for (int j = 0; j < 10; j++)
-// 			cout << "YO";
-// 		i++;
-// 	}
-// 	cout << endl;
-// 	return 0;
-// }
