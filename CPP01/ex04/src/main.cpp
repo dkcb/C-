afaf5d1e7bb56f1e91e65e41c6eaa11d50b60018
dkcb <yes@exa.com>
2023-01-26 22:35:51 +0100

    replace

--- CPP01/ex04/src/main.cpp
+++ CPP01/ex04/src/main.cpp
@@ -1,17 +1,50 @@
-# include <iostream>
+#include <iostream>
+#include <fstream>
+
+std::string	replace(std::string s, std::string rep)
+{
+	int sizer = rep.size();
+	int sizes = s.size();
+	int c = 0;
+
+	while (s.find(rep))
+	{
+		while (c < sizes)
+		{
+			for (int i = 0; i < sizer ; i++)
+				s[s.find(rep) + i] = rep[i];
+			c++;
+		}
+	}
+		std::cout << s << "\n";
+	return (s);
+}
 
 int	main( int argc, char **argv ) {
-	if (argc != 3)
+	if (argc != 4)
 		return 0;
-	std::string brain = "HI THIS IS BRAIN";
-	std::string *stringPTR = &brain;
-	std::string &stringREF = brain;
-
-	std::cout << "The address of brain is: " << &brain;
-	std::cout << "\nThe address of stringPTR is: " << &stringPTR;
-	std::cout << "\nThe address of stringREF is: " << &stringREF;
-	std::cout << "\nThe value of brain is: " << brain;
-	std::cout << "\nThe value of stringPTR is: " << *stringPTR;
-	std::cout << "\nThe value of stringREF is: " << stringREF;
+	std::ifstream src;
+	std::ofstream dest;
+	src.open (argv[1]);
+	dest.open (std::string(argv[1]) + ".replace");
+	if (src.is_open( ) && dest.is_open( ))
+		std::cout << argv[1] << argv[2];
+	else
+		std::cout << "Files opening fail!\n";
+	std::string line;
+	while (!src.eof())
+	{
+		getline (src, line);
+		dest << replace(line, argv[3]) << std::endl;
+		// std::cout << line << "< line | toreplace > " << argv[2] << " with " << argv[3] << "\n";
+	}
+	dest.close( );
+	src.close( );
 	return 0;
 }
+
+		// if (c % sizer == 0 && s.compare(rep) == 0)
+		// {
+			// std::cout << s.substr(c, sizer);
+			// std::cout << "YO!";
+		// }
