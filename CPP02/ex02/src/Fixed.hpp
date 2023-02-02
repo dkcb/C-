45eac05300cd7b1988eb1a07e2b4d7d70e0c2501
dkcb <yes@exa.com>
2023-02-02 15:13:25 +0100

    ex01 ready

---
+++ CPP02/ex02/src/Fixed.hpp
@@ -0,0 +1,24 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+# include <cmath>
+
+class Fixed {
+	public:
+							Fixed(  );
+							~Fixed( );
+							Fixed( const Fixed &nb );
+							Fixed( const int i);
+							Fixed( const float f);
+					Fixed	&operator= ( const Fixed &nb );
+					int		getRawBits( void ) const;
+					void	setRawBits( int const raw );
+					int		toInt( void ) const;
+					float	toFloat( void ) const;
+					friend std::ostream& operator<<(std::ostream &os, const Fixed &dt);
+	private:
+		int					_whole;
+		static const int	_fractional = 8;
+};
+
+#endif
